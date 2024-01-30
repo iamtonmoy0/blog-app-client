f3ff1e7ed152c2d5a29dccbb01cae355a3d7df58
iamtonmoy0 <tonmoy2code@gmail.com>
2024-01-31 01:28:18 +0600

    apollo client added

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import App from "./App.tsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/route.tsx";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>
+  <ApolloProvider client={client}>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </ApolloProvider>
 );
